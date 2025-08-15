50d6decc63be1b09401fe67d785e8de1aa996a1d
Greg Bowler <greg@g105b.com>
2025-08-15 21:45:41 +0100

    wip: style pages

---
+++ page/@code.php
@@ -0,0 +1,37 @@
+<?php
+use Gt\DomTemplate\Binder;
+use Gt\Http\Response;
+use Gt\Input\Input;
+use Gt\Routing\Path\DynamicPath;
+use SizzleLink\EncryptedSecret;
+use SizzleLink\IncorrectDecryptionPasswordException;
+use SizzleLink\SecretNotFoundException;
+
+function go(Input $input, Binder $binder):void {
+	if($error = $input->getString("error")) {
+		$errorMessage = match($error) {
+			"wrong-password" => "The provided password did not match.",
+			default => "There has been an error decrypting your message.",
+		};
+
+		$binder->bindKeyValue("error", $errorMessage);
+	}
+}
+
+function do_view(
+	Input $input,
+	DynamicPath $dynamicPath,
+	Binder $binder,
+	Response $response,
+):void {
+	$password = $input->getString("password");
+	$code = $dynamicPath->get();
+
+	try {
+		$secret = new EncryptedSecret($code);
+		$binder->bindKeyValue("secret", $secret->decrypt($password));
+	}
+	catch(SecretNotFoundException|IncorrectDecryptionPasswordException) {
+		$response->redirect("?error=wrong-password");
+	}
+}
